db26f4aa93bd3712e21067e057276f6207d213d1
abhi9599fds <57456007+abhi9599fds@users.noreply.github.com>
2020-06-21 04:21:56 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='test app',
+    version='1.0.0',
+    description='This is test applicatio',
+    long_description="This is test app",
+    license='Apache-2.0'
+)
